94f61b1e2ad11d5cdd055103b0bb8c599c3ed507
golamrabbi151 <rabbi15-242@diu.edu.bd>
2021-04-17 02:18:13 +0600

    banner add

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './style.scss'
 import { Icon } from 'react-icons-kit'
-import { logIn } from 'react-icons-kit/feather'
+import { logIn, user, facebook, youtube, twitter, alignJustify } from 'react-icons-kit/feather'
 import { Form, FormControl } from 'react-bootstrap'
 import { Image } from '../../utils/Images'
 import { Link } from 'react-router-dom';
@@ -12,18 +12,70 @@ const NavBar = () => {
             <div class="d-flex bd-highlight">
                 <div class="p-2 flex-grow-1 bd-highlight"><img src={Image.Logo} className="logo" alt="logo" /></div>
                 <div class="p-2 bd-highlight">
-                    <Form inline>
+                    {/* <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    </Form>
+                    </Form> */}
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn shadow-sm "
+                    >
+                        <Icon size={'25px'} icon={facebook} />
+                    </Link>
                 </div>
                 <div class="p-2 bd-highlight">
                     <Link
                         to="/login"
                         type="button"
-                        className="btn shadow "
+                        className="btn shadow-sm "
                     >
-                        <Icon size={'35px'} icon={logIn} />
+                        <Icon size={'25px'} icon={youtube} />
+                    </Link>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn shadow-sm "
+                    >
+                        <Icon size={'25px'} icon={twitter} />
+                    </Link>
+                </div>
+                <div class="p-2 bd-highlight">
+                    <Fragment>|</Fragment>
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn shadow-sm btn-active"
+                    >
+                        <Icon size={'25px'} icon={user} />
+                        {/* Login */}
                     </Link> </div>
+            </div>
+            {/* navbar */}
+            {/* <div class="d-flex bd-highlight">
+                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
+                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
+                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
+                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
+                <div class="p-2 bd-highlight">Flex item</div>
+                <div class="p-2 bd-highlight">Third flex item</div>
+            </div> */}
+
+            <div class="d-flex bd-highlight mb-3">
+                <div class="p-2 bd-highlight">
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn shadow-sm "
+                    >
+                        <Icon size={'25px'} icon={alignJustify} />
+                    </Link>
+                </div>
+                <div class="p-2 bd-highlight">Flex item</div>
+                <div class="p-2 bd-highlight">Flex item</div>
+                <div class="p-2 bd-highlight">Flex item</div>
+                <div class="ml-auto p-2 bd-highlight">Flex item</div>
             </div>
         </div>
     );
