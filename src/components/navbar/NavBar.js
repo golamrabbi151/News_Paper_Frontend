e67d961289ff496c660da8d25c512aa3b4be173f
golamrabbi151 <rabbi15-242@diu.edu.bd>
2021-04-20 04:00:17 +0600

    navbar updated

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,7 @@
 import React, { Fragment } from 'react';
 import './style.scss'
 import { Icon } from 'react-icons-kit'
-import { logIn, user, facebook, youtube, twitter, alignJustify } from 'react-icons-kit/feather'
-import { Form, FormControl } from 'react-bootstrap'
+import { user, facebook, youtube, twitter, alignJustify, search } from 'react-icons-kit/feather'
 import { Image } from '../../utils/Images'
 import { Link } from 'react-router-dom';
 
@@ -52,30 +51,30 @@ const NavBar = () => {
                         {/* Login */}
                     </Link> </div>
             </div>
-            {/* navbar */}
-            {/* <div class="d-flex bd-highlight">
-                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
-                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
-                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
-                <div class="p-2 flex-grow-1 bd-highlight">Flex item</div>
-                <div class="p-2 bd-highlight">Flex item</div>
-                <div class="p-2 bd-highlight">Third flex item</div>
-            </div> */}
 
-            <div class="d-flex bd-highlight mb-3">
+            <div class="d-flex bd-highlight mb-3 align-items-center font-weight-bold " >
                 <div class="p-2 bd-highlight">
                     <Link
-                        to="/login"
                         type="button"
-                        className="btn shadow-sm "
+                        className="btn shadow-sm text-dark"
+                        style={{color:"black"}}
                     >
                         <Icon size={'25px'} icon={alignJustify} />
                     </Link>
                 </div>
-                <div class="p-2 bd-highlight">Flex item</div>
-                <div class="p-2 bd-highlight">Flex item</div>
-                <div class="p-2 bd-highlight">Flex item</div>
-                <div class="ml-auto p-2 bd-highlight">Flex item</div>
+                <div class="p-2 bd-highlight">
+                    <Link to="#">বিশেষ সংবাদ</Link>
+                </div>
+                <div class="p-2 bd-highlight">রাজনীতি</div>
+                <div class="p-2 bd-highlight">করোনাভাইরাস</div>
+                <div class="p-2 bd-highlight">বাংলাদেশ</div>
+                <div class="p-2 bd-highlight">বিশ্ব</div>
+                <div class="p-2 bd-highlight">বিনোদন</div>
+                <div class="ml-auto p-2 bd-highlight">
+                <Link >
+                <Icon size={'25px'} icon={search} />
+                </Link>
+                </div>
             </div>
         </div>
     );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,12 @@ const Index = () => {
     return (
        <frames>
            <NavBar />
+<div className="container-fluid p-0  ">
+<div className="shadow-lg " style={{background:"black"}}>
+    <h1 className="text-uppercase text-center p-3" style={{fontWeight:"800"}}>Advertisement</h1>
+</div>
+</div>
+
         <div className="container">
             <h1>Home Page</h1>
         </div>
